97df3820749c6603c1e47d3938841e59cf669e2b
ADRYAN  DERYK TROIANO CHIARARIA <0220832421005@fatectq.local>
2025-05-16 09:31:39 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function MudarVermelho() {
+    document.getElementById('texto').style.color = 'red';
+}
+
+function MudarVerde() {
+    document.getElementById('texto').style.color = 'green';
+}
+
+function MudarAzul() {
+    document.getElementById('texto').style.color = 'blue';
+}
+
+function MudarPreto() {
+    document.getElementById('texto').style.color = '';
+}
